64c5c7ad6262c14ff16804c5e99f6eb7ea90ce39
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-12-02 09:09:50 +0700

    install tailwind and config, rebuild tailwind with .jsx

--- fontend-login/src/pages/user/homepage/HomePage.jsx
+++ fontend-login/src/pages/user/homepage/HomePage.jsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1 className="text-3xl font-bold text-red-400 text-center">HomePage</h1>
     </div>
   );
 };
