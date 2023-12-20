530c7de085f2d0bc46ff5e54cc3788458f48c523
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-12-20 15:01:04 +0700

    mo hinh mvc cho code

--- jwtBackend/src/configs/viewEngine.js
+++ jwtBackend/src/configs/viewEngine.js
@@ -8,11 +8,11 @@ import express from "express";
  */
 const configViewEngine = (app) => {
   /**dinh nghia noi luu tru file */
-  app.use(express.static("/src/public"));
+  app.use(express.static("./src/public"));
   /** dinh nghia cong nghe viet code html voi nodejs */
   app.set("view engine", "ejs");
   /** dinh nghia cac file view se duoc luu o day, giong config trong java luc thi cuoi ky www */
-  app.set("views", "/src/views");
+  app.set("views", "./src/views");
 };
 
 export default configViewEngine;
--- jwtBackend/src/routes/web.js
+++ jwtBackend/src/routes/web.js
@@ -1,14 +1,13 @@
 /**file dieu huong thong thuong se viet o day*/
 
 import express from "express";
-
+import homeController from "../controller/homeController";
 const router = express.Router();
 
 /** khoi tao nhung route trong day */
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello World");
-  });
+  router.get("/", homeController.handleHelloWorld);
+  router.get("/user", homeController.handleUserPage);
 
   return app.use("/", router);
 };
