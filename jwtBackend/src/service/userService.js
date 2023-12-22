340e338b5d219a2e41a8a0f20a68692fd0b8d6fb
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2023-12-22 07:15:51 +0700

    orm create user

--- jwtBackend/src/service/userService.js
+++ jwtBackend/src/service/userService.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import mysql from "mysql2/promise";
 // code theo kieu promise async await
 import bluebird from "bluebird";
+import db from "../models/index";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -11,21 +12,14 @@ const hashUserPassword = (userPassword) => {
 };
 
 const createNewUser = async (email, password, username) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
-  });
-
   let hashPass = hashUserPassword(password);
 
   try {
-    const [rows, fields] = await connection.execute(
-      "INSERT INTO user (email, password, username) VALUES (?,?,?)",
-      [email, hashPass, username]
-    );
-    return rows;
+    await db.User.create({
+      email: email,
+      password: hashPass,
+      username: username,
+    });
   } catch (error) {
     console.log(">>> check error: ", error);
   }
